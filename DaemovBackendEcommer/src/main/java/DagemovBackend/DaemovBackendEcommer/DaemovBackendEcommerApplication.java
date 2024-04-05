81ea42cd127e0e76f1baedb0f210f9f2ac5c32f8
dagemov <sebcoss1234@gmail.com>
2024-04-05 11:06:17 -0400

    initProject

---
+++ DaemovBackendEcommer/src/main/java/DagemovBackend/DaemovBackendEcommer/DaemovBackendEcommerApplication.java
@@ -0,0 +1,13 @@
+package DagemovBackend.DaemovBackendEcommer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemovBackendEcommerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaemovBackendEcommerApplication.class, args);
+	}
+
+}
